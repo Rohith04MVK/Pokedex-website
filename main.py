1b2bfe5b2695b1a2a0efbfbda0b67ae198721187
Rohith04MVK <007rohitjj04@gmail.com>
2020-09-16 19:53:20 +0530

    added first html file

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask.templating import render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return("20-10")
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
