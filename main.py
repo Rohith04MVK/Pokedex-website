64b29b051389c6b7193203e4acb5b238e418293d
Rohith04MVK <007rohitjj04@gmail.com>
2020-09-17 08:47:18 +0530

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask.templating import render_template
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired, ValidationError, InputRequired
+import requests   
 
 app = Flask(__name__)
 
