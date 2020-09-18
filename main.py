e184aa729e755d8ec47bed8b72b3e5aa82fa9767
Rohith04MVK <007rohitjj04@gmail.com>
2020-09-18 14:58:20 +0530

    added forms

--- main.py
+++ main.py
@@ -3,13 +3,24 @@ from flask.templating import render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired, ValidationError, InputRequired
-import requests   
+import requests
+
+
+class Main_form(FlaskForm):
+    pokemon = StringField('Pokemon',
+                          validators=[InputRequired()])
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '*9cnv8to4#64l5mvn8afb2owqldv4!26t4u'
 
-@app.route('/')
+
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    form = Main_form()
+    print(form.pokemon.data)
+    return render_template('index.html', form=form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
