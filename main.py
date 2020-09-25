b7e187f830e90fdd421f0c4084fae54a92aa9ad9
Rohith04MVK <007rohitjj04@gmail.com>
2020-09-25 19:50:54 +0530

    initial commit

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from flask_wtf import form
 from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired, ValidationError, InputRequired
 import requests
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '*9cnv8to4#64l5mvn8afb2owqldv4!26t4u'
 
@@ -31,16 +30,47 @@ def index():
 
 
 def get_stats():
-    url = (f"https://pokeapi.co/api/v2/pokemon/{pokemon}")
-    response = requests.get(url, headers={"Accept": "application/json"})
-    data = response.json()
-    return data
+    r = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}").json()
+
+    # abilities
+    abilities = r["abilities"]
+
+    for ability in abilities:
+        print(f"Ability : {ability['ability']['name']}")
+        print(f"Hidden : {ability['is_hidden']}")
+        print(f"Slot : {ability['slot']}\n")
+
+    print()
+
+    # forms
+    forms = r["forms"]
+
+    for form in forms:
+        print(f"Form name: {form['name']}\n")
+
+    print()
+
+    # species
+    species = r["species"]
+
+    print(f"Species name: {species['name']}\n\n")
+
+    # types
+    types = r["types"]
+
+    for poke_type in types:
+        print(f"Slot number: {poke_type['slot']}")
+        print(f"Type name: {poke_type['type']['name']}\n")
 
 
 @app.route("/info", methods=["GET", "POST"])
 def info():
-    return get_stats()
+    pok = pokemon
+    da = get_stats()
+    return render_template("info.html", pok=pok, da=da)
 
+
+get_stats()
 
 if __name__ == "__main__":
     app.run(debug=True)
